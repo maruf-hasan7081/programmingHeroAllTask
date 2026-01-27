c7f7fd59160021648b90108b4bd92f75eaa9bd4d
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-28 00:38:44 +0600

    numberconvert

---
+++ Milestone2/module13/number.js
@@ -0,0 +1,26 @@
+var number=5;
+var number2=10;
+var number3= parseInt('15');
+
+
+//convert korte parsetype use korte hoi jmn string k int e convert korbo tahole amke parseInt() use korte hobe 
+console.log(number);
+console.log(number2);
+
+// console.log(number+number2);
+console.log(number+number3);
+
+var first =0.1;
+var second =0.2;
+var total = first+second;
+console.log(total.toFixed(3));
+//toFixed() use kore doshomik er por koi gor nibo ta ta bole dibo
+
+// kintu toFixed() use korle output ta kintu string e ashbe 
+
+// so ekhn amra korbo ki -----
+
+console.log(parseFloat(total.toFixed(3)) );
+
+
+
