aacd78a087c5b1812ef02cc0f4d596e31ba0ab68
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-27 20:06:19 +0600

    variable

---
+++ Milestone2/module13/variable.js
@@ -0,0 +1,11 @@
+// variable
+
+var age=10;
+age=100;
+age=24;
+var name ="maruf";
+name="maruf hasan"
+name="maruf hasan chowdhury";
+name="MD. MARUF HASAN CHOWDHURY"
+ console.log(name);
+ console.log(age);
