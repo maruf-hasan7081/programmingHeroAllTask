50e45d26ce01e7fd4fac563d6778660f84b9d0c6
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-28 02:15:03 +0600

    task2

--- Milestone2/module13/task.js
+++ Milestone2/module13/task.js
@@ -1,5 +1,22 @@
-var totalTaka=1000;
-var orangesPrice=200;
-var applePrice=300;
-var totalPrice=orangesPrice+applePrice;
-console.log("reture money:", totalTaka-totalPrice);
+// task1 
+
+// var totalTaka=1000;
+// var orangesPrice=200;
+// var applePrice=300;
+// var totalPrice=orangesPrice+applePrice;
+// console.log("reture money:", totalTaka-totalPrice);
+
+// task2
+
+
+var math=75.25;
+var biology=65;
+var chemistry=80;
+var physics=35.45;
+var bangla=99.50;
+
+var totalMarks= math+ biology+ chemistry+ physics+bangla;
+
+console.log((totalMarks/5).toFixed(2));
+
+
