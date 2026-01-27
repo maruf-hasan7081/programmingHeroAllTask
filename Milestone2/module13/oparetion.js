aa756b66974e28e18300e06bb2b8dff3eb675ee7
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-28 01:10:15 +0600

    oparetion and shorthand

---
+++ Milestone2/module13/oparetion.js
@@ -0,0 +1,31 @@
+// var num =10
+// //num= num + 10;
+// num +=10; // shorthand
+// console.log(num)
+
+// var num =20;
+// num=num-10;
+// // num +=10; // shorthand
+// console.log(num)
+
+// var num =20;
+// // num=num*10;
+//  num *=10; // shorthand
+// console.log(num)
+
+
+// var firstName= "maruf";
+// var lastName = "Chowdhury";
+
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+
+var num1 = 50;
+var num2 = '10';
+// console.log(num1+num2);   // ekhne shudu +oparetion er khetre string hisebe count hoi ....bki gula thik thak
+console.log(num1-num2);
+
+
+
