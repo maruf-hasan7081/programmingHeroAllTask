5d863269d249687c9b5f22062043861d96fc90d9
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-28 04:55:31 +0600

    condition

---
+++ Milestone2/module14/comparison.js
@@ -0,0 +1,66 @@
+/**
+ * comparison operator
+ * 1.bigger: >
+ * 2.less: <
+ * 3.equal: ==
+ * 4.not equal: !=
+ * 5. greater then or equal: >=
+ * 6. less then or equal: <=
+ * 
+ * 7. and: &&
+ * 8. or: ||
+ * 
+ */
+
+// condition
+
+/**  
+if(condition){
+    // code execution
+}
+else{
+    // code execution
+}
+    */ 
+
+
+const salary = 5000;
+const hight=60
+
+// if(salary >= 5000 && hight >= 60){
+//     console.log('i am rich');
+// }
+// else{
+//     console.log('i am poor');
+// }
+
+if(salary >= 5000 || hight >= 60){
+    console.log('i am rich');   
+}
+else{
+    console.log('i am poor');
+}
+
+
+// simple ternary operator
+
+// condition ? true : false
+const myAge = 15;
+(myAge >= 18) ? console.log('i am adult') : console.log('i am not adult');
+
+// behaivor control korar jonno condition use koriii
+
+// nested if else
+// if(condition){
+//     if(condition){
+//         // code execution
+//     }
+//     else{
+//         // code execution
+//     }
+// }
+// else{
+//     // code execution
+// }
+
+
