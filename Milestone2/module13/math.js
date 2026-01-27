93f99316f61b482450094e8d84d79e6424e7c8bc
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-27 19:32:09 +0600

    math

---
+++ Milestone2/module13/math.js
@@ -0,0 +1,39 @@
+// // 1.number: +ve ,-ve ,0
+// // 2.integer: 1,2,3,4,5,6,7,8,9,10
+// // 3.float: 1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.10
+// // 4.operation:+ , -, *, /, %, ++, --, +=, -=, *=, /=, %=
+// 5.percentage= %
+// 6.sum=a+b
+// 7.subtraction=a-b
+// 8.multiplication=a*b
+// 9.division=a/b
+// 10.modulus=a%b
+// 11.increment=a++
+// 12.decrement=a--
+// 13.addition=a+=b
+// 14.subtraction=a-=b
+// 15.multiplication=a*=b
+// 16.division=a/=b
+// 17.modulus=a%=b
+// 18.avg= (a+b+c)/3
+// 19.max=max(a,b,c)
+// 20.min=min(a,b,c)
+// 21.area: triangle=0.5*b*h
+// 22.area: rectangle=l*b
+// 23.area: circle=3.14*r*r
+// 24.area: square=s*s
+// series: 1,2,3,4,5,6,7,8,9,10
+// series: 1,3,5,7,9,11,13,15,17,19 
+// series: 2,4,6,8,10,12,14,16,18,20
+// series formula: a+(n-1)*d
+// series: 1,4,9,16,25,36,49,64,81,100
+// series formula: a*n*n
+// series: 1,8,27,64,125,216,343,512,729,1000
+// series formula: a*n*n*n
+
+
+// const a=10;
+// const b=20;
+// const c=30;
+// const d=40;
+// const e=
