288e31fbce636465abf520036fbc84bbc199e835
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-27 21:00:45 +0600

    type

--- Milestone2/module13/variable.js
+++ Milestone2/module13/variable.js
@@ -9,3 +9,16 @@ name="maruf hasan chowdhury";
 name="MD. MARUF HASAN CHOWDHURY"
  console.log(name);
  console.log(age);
+
+ var isans=true;
+ console.log(isans);
+
+
+ // type check
+
+ //tahole amra kono varibale er type check korbo typeof diye ......oi name tar age shudu typeof diye dibo taholei ok 
+ 
+
+ console.log(typeof name);
+ console.log(typeof age);
+ console.log(typeof isans);
