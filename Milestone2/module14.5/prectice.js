029283f6f4d64e0a0246cc922a0abeb6fd44d5ca
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-30 20:09:38 +0600

    task2

--- Milestone2/module14.5/prectice.js
+++ Milestone2/module14.5/prectice.js
@@ -1,6 +1,18 @@
+/*
+//task1-5 furits, 3rd index element , and 2nd element replece with jambura and print the array
 const fruits=['apple' ,'orange' , 'am' ,'kathal', 'fineapple']
 console.log(fruits);
 console.log(fruits[3]);
 
 fruits[2]='jambura';
 console.log(fruits);
+
+*/
+
+// task2
+
+const touristPlace=['dhaka' , 'chittagong' , 'coxsbazar'];
+touristPlace.push('sreemangal');
+touristPlace.push('cumilla' , 'barishal', 'khulna');
+touristPlace.pop();
+console.log(touristPlace);
