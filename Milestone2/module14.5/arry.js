1197bc47e297ec9e9b30df7e98a1bed398174425
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-30 17:41:30 +0600

    push-pop,shift-unshift

--- Milestone2/module14.5/arry.js
+++ Milestone2/module14.5/arry.js
@@ -13,7 +13,39 @@
 
  //update element
 
- number[1]=50;
- console.log(number[1]);
- console.log(number);
+//  number[1]=50;
+//  console.log(number[1]);
+//  console.log(number);
+
+
+
+// push- pop.......push() diye element add korii....pop() diye element remove korii.....pop() diylei last elemenet delete hooye jai ....bt push(er bithore element dite hobe ) tokhn last e giye add hobe 
+
+//push
+ const number1 =[12,13,14,15,16,17,18,19,20];
+
+ number1.push(222);   //last e giye add hobe 
+
+ console.log(number1);
+
+ //pop
+
+ number1.pop();  //last e giye delete hobe...() er bithore kicui dewa lage na 
+ console.log(number1);
+
+
+
+ //shift-unshift........shift() diye first element delete hobe ....unshift() diye first e giye add hobe
+
+
+ //shift
+ const number2 =[12,13,14,15,16,17,18,19,20];
+
+ number2.shift();
+ console.log(number2) ; //first e giye delete hobe
+
+ //unshift
+ number2.unshift(111); //first e giye add hobe
+ console.log(number2);
+
 
