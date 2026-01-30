5d47a00f52dd147ee90e549d7a487e98311180cd
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-30 19:18:31 +0600

    include,indexof,isArray

--- Milestone2/module14.5/arry.js
+++ Milestone2/module14.5/arry.js
@@ -1,51 +1,72 @@
- const number =[12,13,14,15,16,17,18,19,20];
+//  const number =[12,13,14,15,16,17,18,19,20];
 
-//  console.log(number);
+// //  console.log(number);
 
- //index
+//  //index
 
-//  console.log(number[5])
+// //  console.log(number[5])
 
- //get element
+//  //get element
 
-//  const fourth=number[5];
-//  console.log(fourth);
+// //  const fourth=number[5];
+// //  console.log(fourth);
 
- //update element
+//  //update element
 
-//  number[1]=50;
-//  console.log(number[1]);
-//  console.log(number);
-
-
-
-// push- pop.......push() diye element add korii....pop() diye element remove korii.....pop() diylei last elemenet delete hooye jai ....bt push(er bithore element dite hobe ) tokhn last e giye add hobe 
-
-//push
- const number1 =[12,13,14,15,16,17,18,19,20];
-
- number1.push(222);   //last e giye add hobe 
-
- console.log(number1);
-
- //pop
-
- number1.pop();  //last e giye delete hobe...() er bithore kicui dewa lage na 
- console.log(number1);
+// //  number[1]=50;
+// //  console.log(number[1]);
+// //  console.log(number);
 
 
 
- //shift-unshift........shift() diye first element delete hobe ....unshift() diye first e giye add hobe
+// // push- pop.......push() diye element add korii....pop() diye element remove korii.....pop() diylei last elemenet delete hooye jai ....bt push(er bithore element dite hobe ) tokhn last e giye add hobe 
+
+// //push
+//  const number1 =[12,13,14,15,16,17,18,19,20];
+
+//  number1.push(222);   //last e giye add hobe 
+
+//  console.log(number1);
+
+//  //pop
+
+//  number1.pop();  //last e giye delete hobe...() er bithore kicui dewa lage na 
+//  console.log(number1);
 
 
- //shift
- const number2 =[12,13,14,15,16,17,18,19,20];
 
- number2.shift();
- console.log(number2) ; //first e giye delete hobe
+//  //shift-unshift........shift() diye first element delete hobe ....unshift() diye first e giye add hobe
 
- //unshift
- number2.unshift(111); //first e giye add hobe
- console.log(number2);
+
+//  //shift
+//  const number2 =[12,13,14,15,16,17,18,19,20];
+
+//  number2.shift();
+//  console.log(number2) ; //first e giye delete hobe
+
+//  //unshift
+//  number2.unshift(111); //first e giye add hobe
+//  console.log(number2);
+
+
+// include()...........include() diye kono element ache kina check korbo amra  so ekhn amra try kore dekhi
+
+
+const number =[12,13,14,15,16,17,18,19,20];
+console.log(number.includes(12));
+console.log(number.includes(32));
+
+
+// indexOf()...........indexOf() diye kono element koto no index e ache ta check korbo
+
+console.log(number.indexOf(12));
+console.log(number.indexOf(32));
+
+
+const a=5;
+console.log(Array.isArray(a));
+console.log(Array.isArray(number));
+
+
 
 
