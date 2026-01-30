e21309cf78c9f7d39e26cf7c013b6632bed3e543
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-30 20:25:45 +0600

    task3,4,5

--- Milestone2/module14.5/prectice.js
+++ Milestone2/module14.5/prectice.js
@@ -1,18 +1,39 @@
-/*
-//task1-5 furits, 3rd index element , and 2nd element replece with jambura and print the array
-const fruits=['apple' ,'orange' , 'am' ,'kathal', 'fineapple']
-console.log(fruits);
-console.log(fruits[3]);
+// /*
+// //task1-5 furits, 3rd index element , and 2nd element replece with jambura and print the array
+// const fruits=['apple' ,'orange' , 'am' ,'kathal', 'fineapple']
+// console.log(fruits);
+// console.log(fruits[3]);
 
-fruits[2]='jambura';
-console.log(fruits);
+// fruits[2]='jambura';
+// console.log(fruits);
 
-*/
+// */
 
-// task2
+// // task2
 
-const touristPlace=['dhaka' , 'chittagong' , 'coxsbazar'];
-touristPlace.push('sreemangal');
-touristPlace.push('cumilla' , 'barishal', 'khulna');
-touristPlace.pop();
-console.log(touristPlace);
+// const touristPlace=['dhaka' , 'chittagong' , 'coxsbazar'];
+// touristPlace.push('sreemangal');
+// touristPlace.push('cumilla' , 'barishal', 'khulna');
+// touristPlace.pop();
+// console.log(touristPlace);
+
+
+//task3
+
+const book=['bangla' , 'english' , 'bangla' , 'math'];
+console.log(book.includes('bangla'));
+console.log(book.includes('java'));
+
+//task4
+const a=5;
+const b=[];
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
+
+//task5
+
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+const number2=[11,12,13,14,15,16,17,18,19,20];
+console.log(numbers);
+console.log(number2);
+console.log(numbers.concat(number2));
