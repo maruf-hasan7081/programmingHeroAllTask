00c7c6083ddd99697158a226bea1ddc0cc2c86b9
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-31 16:22:45 +0600

    while loop

--- Milestone2/module15/while.js
+++ Milestone2/module15/while.js
@@ -1,10 +1,38 @@
-let num=1;
+// let num=1;
 
-while(num<10){
-    console.log("number is :" , num);
-    // num=num+1;
-    // num+=1;
+// while(num<10){
+//     console.log("number is :" , num);
+//     // num=num+1;
+//     // num+=1;
 
-    num++;
+//     num++;
+
+// }
+
+//print sum 1to 10
+
+let number=1;
+let sum=0
+
+while(number<=10){
+    
+    sum=sum+number;
+    number++;
+    
+}
+console.log(sum); 
+
+//odd even number
+let number1=1;
+
+while(number1<=10){
+    // console.log(number1);
+    if(number1%2==0){
+        console.log("the even number is :" , number1);
+    }
+    else{
+        console.log("the odd number is :" , number1);
+    }
+    number1++;
 
 }
