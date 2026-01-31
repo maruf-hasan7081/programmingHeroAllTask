43c944b79b7fde485375a043057c2585e15ff48c
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-01-31 17:23:55 +0600

    for-loop

---
+++ Milestone2/module15/for-loop.js
@@ -0,0 +1,27 @@
+// //syntex
+
+// for(    ;     ;     ){
+//     //code
+// }
+
+
+// for (first parent(loop variable) ; second parent(condition) ; third parent(increment/decrement) ){
+//     //code
+// }
+
+//for loop
+
+for (let i=0; i<=10; i++){
+    console.log(i);
+}
+
+//even
+for(let i=0; i<10 ; i+=2) // i+=2 means i=i+2 also calle shorthand operator
+{
+    console.log(i);
+}
+// odd
+for(let i=1; i<10 ; i+=2) // i+=2 means i=i+2 also calle shorthand operator
+{
+    console.log(i);
+}
