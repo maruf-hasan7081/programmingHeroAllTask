cbf086585cd5a673e3abee22643959c8038e07ff
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-02-02 18:06:13 +0600

    add

---
+++ Milestone2/module15/prec-loop.js
@@ -0,0 +1,70 @@
+// /***
+
+// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+//  */
+
+// for(let i=1 ; i<=60 ; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!" , i)
+// }
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+// for(let i=61 ; i<=100 ; i+=2){
+//     console.log(i)
+
+// }
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+//  */
+// for(let i=78 ; i<=98; i+=2){
+//     console.log(i)
+// }
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+
+// let total=0;
+// for(let i=91; i<=129 ; i+=2){
+//     total=total+i;
+    
+// }
+// console.log(total);
+
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+// for(let i=1 ; i<=10 ; i++){
+//     console.log("9 x ", i , " = " , 9*i)
+// }
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+for(let i=81 ; i>=65 ; i--){
+    console.log(i)
+}
