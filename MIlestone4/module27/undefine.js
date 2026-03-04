13e74064f71922ea1ce61609f0262dc326528a68
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-03-04 19:08:24 +0600

    undefineVSnull

---
+++ MIlestone4/module27/undefine.js
@@ -0,0 +1,17 @@
+// const sum=(x,y)=>{
+//     console.log(x,y);}
+
+
+//     sum();
+
+
+const student={
+    name:"maruf",
+    age:null,
+    address:"dhaka"
+}
+console.log(student.name);
+console.log(student.marks);
+console.log(student.age);
+
+
