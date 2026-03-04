9ce5aa95473afac265eab6363fe205f093211899
MD. Maruf Hasan Chowdhury <marufhasanchowdhury06@gmail.com>
2026-03-04 11:10:49 +0600

    spread

---
+++ MIlestone4/modeul26/Spread.js
@@ -0,0 +1,24 @@
+let number = [1,2,3,4,5,6,7,8,9,10];
+let number2=[...number,25,26,27,28,29,30];
+
+console.log(...number2);
+
+
+
+let nam={
+    name:"maruf",
+    age:25,
+    address:"dhaka"
+}
+
+// console.log(nam);
+let nam2={
+    ...nam,
+    name:"maruf2",
+    age:26,
+    address:"dhaka"
+}
+
+console.log(nam2);
+
+
