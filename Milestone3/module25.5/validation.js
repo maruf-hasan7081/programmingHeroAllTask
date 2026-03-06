585e8092b7fba2f3aeadf5a36b666be4e6e0ae64
maruf-hasan7081 <marufhasanchowdhury06.com>
2026-03-06 09:04:38 +0600

    ok

---
+++ Milestone3/module25.5/validation.js
@@ -0,0 +1,15 @@
+const num =23;
+
+if(num>=23){
+
+    if(num!==isNaN(num)){
+        console.log(' it is num')
+
+    }
+    else{
+        console.log("it is a string")
+    }
+}
+else{
+    console.log("logic not match")
+}
